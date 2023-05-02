ac3e2cd3fd703ed9897f8579ed75e2edc16b2f36
dudushy <eduardotalarico1337@gmail.com>
2023-05-02 20:48:05 -0300

    trying to read file

--- OCR/Program.cs
+++ OCR/Program.cs
@@ -4,14 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Tesseract;
+using System.Windows.Forms;
+using System.IO;
 
 namespace OCR
 {
     internal class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var dialog = new OpenFileDialog
+            {
+                Multiselect = false,
+                Title = "Please select a image",
+                Filter = "Image|*.png;*.jpeg;*.jpg"
+            };
+
+            using (dialog)
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var imgPath = dialog.FileName;
+                    Console.WriteLine("imgPath: " + imgPath);
+                }
+            }
         }
     }
 }
