8012543c10399a1144911d40ac97774cf8a294db
dudushy <eduardotalarico1337@gmail.com>
2023-05-03 19:02:47 -0300

    Update Form1.cs

--- OCR/Form1.cs
+++ OCR/Form1.cs
@@ -1,14 +1,27 @@
+using Tesseract;
+
 namespace OCR
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        const string TITLE = "Form1";
+
+        public string imagePath;
+
+        public Form1(string imagePath)
         {
+            System.Diagnostics.Debug.WriteLine($"[{TITLE}#Form1]");
+
             InitializeComponent();
+
+            this.imagePath = imagePath;
+            this.labelFilename.Text = "Filename: " + this.imagePath;
         }
 
         private void buttonSelectFile_Click(object sender, EventArgs e)
         {
+            System.Diagnostics.Debug.WriteLine($"[{TITLE}#buttonSelectFile_Click] --START");
+
             this.openFileDialog1 = new OpenFileDialog
             {
                 Multiselect = false,
@@ -20,18 +33,48 @@ namespace OCR
             {
                 if (openFileDialog1.ShowDialog() == DialogResult.OK)
                 {
-                    var imgPath = openFileDialog1.FileName;
-                    System.Diagnostics.Debug.WriteLine("imgPath: " + imgPath);
+                    this.imagePath = openFileDialog1.FileName;
+                    System.Diagnostics.Debug.WriteLine($"[{TITLE}#buttonSelectFile_Click] imagePath: " + this.imagePath);
 
-                    this.labelFilename.Text = imgPath;
-                    this.pictureBox1.Load(imgPath);
+                    this.labelFilename.Text = "Filename: " + this.imagePath;
+                    this.pictureBox1.Load(this.imagePath);
                 }
             }
+
+            System.Diagnostics.Debug.WriteLine($"[{TITLE}#buttonSelectFile_Click] --STOP");
         }
 
         private void buttonRun_Click(object sender, EventArgs e)
         {
+            System.Diagnostics.Debug.WriteLine($"[{TITLE}#buttonRun_Click] --START");
 
+            System.Diagnostics.Debug.WriteLine($"[{TITLE}#buttonRun_Click] imagePath: " + this.imagePath);
+
+            try
+            {
+                using (var engine = new TesseractEngine(@"tessdata", "eng", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(this.imagePath))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            var text = page.GetText();
+                            System.Diagnostics.Debug.WriteLine($"[{TITLE}#buttonRun_Click] text: " + text);
+
+                            var precisionRate = page.GetMeanConfidence();
+                            System.Diagnostics.Debug.WriteLine($"[{TITLE}#buttonRun_Click] precisionRate: " + precisionRate);
+
+                            this.textBoxOutput.Text = text;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"[{TITLE}#buttonRun_Click] Exception: " + ex.Message);
+            }
+
+            System.Diagnostics.Debug.WriteLine($"[{TITLE}#buttonRun_Click] --STOP");
         }
     }
 }
